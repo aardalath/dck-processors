3fd72d4cdefd1b2853632c801fde3996db64e3b8
J C Gonzalez <JCGonzalez@sciops.esa.int>
2017-10-20 09:40:18 +0200

    Initial commit

---
+++ old/DummyProcessor/kkk/kkk.py
@@ -0,0 +1,11 @@
+import os
+
+
+class KKK(object):
+    '''
+    Class KKK
+    '''
+    def __init__(self):
+        curDir = os.path.realpath(__file__)
+        print "Current dir is {}".format(curDir)
+
---
+++ old/DummyProcessor/testkkk.py
@@ -0,0 +1,10 @@
+from kkk.kkk import KKK
+import os
+
+def main():
+    k = KKK()
+    print os.path.realpath(__file__)
+
+if __name__ == "__main__":
+    main()
+
